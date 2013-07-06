5e7415fe50cf42622f07fa3f2773661674c97466
Alex Pierstoval <pierstoval@gmail.com>
2013-07-06 23:29:31 +0200

    Ajout de Git au traçage des logs

--- pages/mod_admin_errors.php
+++ pages/mod_admin_errors.php
@@ -9,6 +9,7 @@ $types_ok = array(
 	'exectime' => 'Temps d\'exécution',
 	'error_tracking' => 'Traçage des erreurs',
 	'referer' => 'Différents referer',
+	'git' => 'Messages Git',
 );
 
 $arr = array();
