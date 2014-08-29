6d66d829e162e201f5d91f7f0c21d01799562560
Alex Pierstoval <pierstoval@gmail.com>
2014-08-29 20:40:15 +0200

    Evolution #10 - Choix des revers => Bugfix sur le bouton "Étape Suivante" qui ne fonctionnait pas correctement après avoir tiré des revers

--- modules_create_char/mod_07_revers.php
+++ modules_create_char/mod_07_revers.php
@@ -1,19 +1,19 @@
 <?php
 
-	$t = $db->req('SELECT %rev_id,%rev_name,%rev_desc FROM %%revers ORDER BY %rev_id ASC');
-	$revers = array();
-	foreach ($t as $k => $v) {
-		$revers[$v['rev_id']] = $v;
-	}
-	unset($revers[1], $t, $k, $v);
+    $t = $db->req('SELECT %rev_id,%rev_name,%rev_desc FROM %%revers ORDER BY %rev_id ASC');
+    $revers = array();
+    foreach ($t as $k => $v) {
+        $revers[$v['rev_id']] = $v;
+    }
+    unset($revers[1], $t, $k, $v);
 
-	$age = isset($_SESSION[$steps[6]['mod']]) ? $_SESSION[$steps[6]['mod']] : false;
-	if ($age === false) {
-		tr('L\'âge n\'a pas été défini, merci de vous rendre à l\'étape correspondante.');
-		echo '<br />';
-		mkurl(array('params'=>6, 'type' => 'tag', 'anchor' => 'Aller à la page des Voies', 'attr' => 'class="btn"'));
-		return;
-	}
+    $age = isset($_SESSION[$steps[6]['mod']]) ? $_SESSION[$steps[6]['mod']] : false;
+    if ($age === false) {
+        tr('L\'âge n\'a pas été défini, merci de vous rendre à l\'étape correspondante.');
+        echo '<br />';
+        mkurl(array('params'=>6, 'type' => 'tag', 'anchor' => 'Aller à la page des Voies', 'attr' => 'class="btn"'));
+        return;
+    }
 
     $nb_revers = 0;
     if ($age > 20) { $nb_revers ++; }
@@ -23,10 +23,10 @@
     $choose_manually = null;
     $poisse = false;
 
-	if ($nb_revers === 0) {
+    if ($nb_revers === 0) {
         $dice = array(0=>0);
     } else {
-		if (!$p_stepval) {
+        if (!$p_stepval) {
             if (isset($_GET['manually'])) {
                 if ($_GET['manually'] === 'false') {
                     // Calcul automatique
@@ -55,6 +55,8 @@
                         } while (in_array($act, $dice));
                         $dice[3] = $act;
                     }
+                    Session::write($page_mod, $dice);
+                    redirect(mkurl(array('params'=>$steps[$page_step]['mod'])));
                 } elseif ($_GET['manually'] !== 'true') {
                     // Paramètre invalide
                     $choose_manually = true;
@@ -76,6 +78,8 @@
                                 $dice[] = $setback;
                             }
                         }
+                        Session::write($page_mod, $dice);
+                        redirect(mkurl(array('params'=>$steps[$page_step]['mod'])));
                     } else {
                         // Message d'erreur sur la quantité de revers choisis
                         redirect(mkurl(array(
@@ -92,30 +96,31 @@
                     redirect(mkurl(array('params'=>$steps[$page_step]['mod'])), tr('Erreur dans le formulaire.', true), 'error');
                 }
             }
-		} else {
-			$dice = $p_stepval;
-		}
+        } else {
+            $dice = $p_stepval;
+        }
 
 
-	}
-	if ($nb_revers === 0 || $age < 21) {
+    }
+    if ($nb_revers === 0 || $age < 21) {
         $dice = array(0=>0);
-		$_SESSION[$page_mod] = array(0=>0);
-		if (!$p_stepval && P_DEBUG === false) {
-			$_SESSION['etape']++;
+        $_SESSION[$page_mod] = array(0=>0);
+        if (!$p_stepval && P_DEBUG === false) {
+            $_SESSION['etape']++;
             redirect(mkurl(array('params'=>$steps[$page_step+1]['mod'])));
-		}
-	}
-	if (empty($p_stepval) && isset($dice)) {
-		Session::write($page_mod, $dice);
-		$_SESSION['etape'] = $page_step+1;
-		$p_stepval = $dice;
-	}
-	?>
-	<div class="row">
-		<div class="span" id="revers">
-		<?php
-		if ($p_stepval) {
+        }
+    }
+    if (empty($p_stepval) && isset($dice)) {
+        Session::write($page_mod, $dice);
+        $_SESSION['etape'] = $page_step+1;
+        $p_stepval = $dice;
+    }
+    ?>
+    <div class="row">
+        <div class="span" id="revers">
+        <?php
+        if ($p_stepval) {
+            echo '<input type="hidden" id="chosen" value="true" />';
             if (isset($p_stepval[0]) && $p_stepval[0] == 0) {
                 ?>
                 <p><?php tr('Aucun revers (moins de 21 ans)'); ?></p>
@@ -137,7 +142,8 @@
                     }
                 }
             }
-		} else {
+        } else {
+            echo '<input type="hidden" id="chosen" value="false" />';
             if ($choose_manually === null || $choose_manually === true) { ?>
                 <p class="alert alert-info">
                     <?php tr('Votre personnage a <strong>%age%</strong> ans, il doit donc tirer <strong>%nb_revers%</strong> revers.', false, array('%age%' => $age, '%nb_revers%' => $nb_revers)); ?><br />
@@ -162,40 +168,47 @@
                     } ?>
                     <input type="hidden" id="nb_revers" value="<?php echo $nb_revers; ?>" />
                     <p class="well well-small"><?php tr('Reste %remaining% revers à choisir.', false, array('%remaining%' => '<strong id="remaining_setbacks">'.$nb_revers.'</strong>')); ?></p>
-                    <button id="send_setbacks" type="submit" class="btn btn-success" style="display: none;"><?php tr('Envoyer'); ?></button>
                 </form><?php
             }
-		}
-		unset($dice);
-		?>
-		</div>
-	</div>
+        }
+        unset($dice);
+        ?>
+        </div>
+    </div>
 
-	<?php
+    <?php
 echo "<style type=\"text/css\">#gen_send{visibility:visible;}</style>";
 buffWrite('css', '
-	#formgen p#generated { display: none; }
-	#formgen div[class*="span"]:hover { cursor: default; }
+    #formgen p#generated { display: none; }
+    #formgen div[class*="span"]:hover { cursor: default; }
 ', $page_mod);
 buffWrite('js', "
-	$(document).ready(function(){
-	    var nb_revers_max = parseInt($('#nb_revers').val());
-	    $('.input_setback_manual').on('change', function(){
-	        var nb_revers = $('.input_setback_manual:checked').length;
-	        if (nb_revers > nb_revers_max) {
-	            this.checked = false;
-	            nb_revers--;
+    $(document).ready(function(){
+        var nb_revers_max = parseInt($('#nb_revers').val());
+        $('.input_setback_manual').on('change', function(){
+            var nb_revers = $('.input_setback_manual:checked').length;
+            if (nb_revers > nb_revers_max) {
+                this.checked = false;
+                nb_revers--;
                 \$(this).removeAttr('checked').prop('checked', false);
-	        }
-	        if (nb_revers == nb_revers_max) {
-                $('#send_setbacks').slideDown(400);
-	        } else {
-	            $('#send_setbacks').slideUp(400);
-	        }
-	        document.getElementById('remaining_setbacks').innerHTML = nb_revers_max - nb_revers;
-	    });
-	    $('#choose_manually').on('click', function(){
-	        $('#setbacks_list').slideDown(400);
-	    });
-	});
+            }
+            if (nb_revers == nb_revers_max) {
+                \$('#gen_send').attr('href', '#').css('visibility','visible').on('click', function(){\$('#setbacks_list').submit();});
+            } else {
+                \$('#gen_send').attr('href', '#').css('visibility','hidden').on('click', function(){\$('#setbacks_list').submit();});
+            }
+            document.getElementById('remaining_setbacks').innerHTML = nb_revers_max - nb_revers;
+        });
+        $('#choose_manually').on('click', function(){
+            $('#setbacks_list').slideDown(400);
+            $(this).slideUp(400);
+        });
+        if (document.getElementById('chosen').value === 'true') {
+            values.etape = ".$page_step.";
+            values['".$page_mod."'] = $('#age_val').text();
+            sendMaj(true, '".$p_action."');
+        } else {
+            \$('#gen_send').attr('href', '#').css('visibility','hidden');
+        }
+    });
 ", $page_mod);
