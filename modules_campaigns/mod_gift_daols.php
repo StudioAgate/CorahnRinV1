db3d8408182326a36090b0da1b81508dfa8897cd
Alex Pierstoval <pierstoval@gmail.com>
2015-08-30 15:29:02 +0200

    Minor html fix on daols module

--- modules_campaigns/mod_gift_daols.php
+++ modules_campaigns/mod_gift_daols.php
@@ -4,19 +4,19 @@ $daols = $char->get_daols();
 
 	<div class="m10">
 		<div class="control-group">
-			<label for="exp" class="control-label"><?php tr('Daols de braise'); ?></label>
+			<label for="daols_braise" class="control-label"><?php tr('Daols de braise'); ?></label>
 			<div class="controls">
 				<input id="daols_braise" name="daols_braise" type="text" class="input-mini" value="<?php echo $daols['braise']; ?>" />
 			</div>
 		</div>
 		<div class="control-group">
-			<label for="exp" class="control-label"><?php tr('Daols d\'azur'); ?></label>
+			<label for="daols_azur" class="control-label"><?php tr('Daols d\'azur'); ?></label>
 			<div class="controls">
 				<input id="daols_azur" name="daols_azur" type="text" class="input-mini" value="<?php echo $daols['azur']; ?>" />
 			</div>
 		</div>
 		<div class="control-group">
-			<label for="exp" class="control-label"><?php tr('Daols de givre'); ?></label>
+			<label for="daols_givre" class="control-label"><?php tr('Daols de givre'); ?></label>
 			<div class="controls">
 				<input id="daols_givre" name="daols_givre" type="text" class="input-mini" value="<?php echo $daols['givre']; ?>" />
 			</div>
