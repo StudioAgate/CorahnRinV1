a70cc82683a9886250e58d8029db963fb41aa346
Alex Rock Ancelet <pierstoval@gmail.com>
2018-06-01 22:21:36 +0200

    Code style on bdd class

--- class/class_bddPDO.php
+++ class/class_bddPDO.php
@@ -6,7 +6,8 @@
  * Effectue des requêtes préparées
  * Simplifie les noms de tables avec les préfixes %% et % (voir méthode buildReq)
  */
-class bdd {
+class bdd
+{
 
     public static $prefix;
     private $db;
@@ -20,32 +21,44 @@ class bdd {
     public $queriesRunnedCount = 0;
     public $queriesRunned = [];
 
-    public function __construct($host = '127.0.0.1', $user = 'root', $pwd = '', $database = 'mydb', $tb_prefix = '', $db_type = 'mysql') {
+    public function __construct(
+        $host = '127.0.0.1',
+        $user = 'root',
+        $pwd = '',
+        $database = 'mydb',
+        $tb_prefix = '',
+        $db_type = 'mysql'
+    ) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
         self::$prefix = $tb_prefix;
-        $dsn = $db_type.':host=' . $host . ';dbname=' . $database . '';
+        $dsn = $db_type.':host='.$host.';dbname='.$database.'';
         $this->initErr(true);
         try {
             $this->db = new PDO($dsn, $user, $pwd, $pdo_options);
             $this->dbname = $database;
         } catch (Exception $e) {
             $this->showErr($e, null, null, true);
-            return null;
+
+            return;
         }
         //$this->initErr(false);
         $this->noRes('SET NAMES "utf8"');
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         $ret = '';
-        $ret .= p_dump(array(
-            'database' => $this->dbname,
-            'show_err' => $this->show_err,
-            'err_type' => $this->err_type,
-            'last_query' => $this->last_query,
-            'last_values' => $this->last_values,
-            'last_results' => $this->last_results
-        ));
+        $ret .= p_dump(
+            [
+                'database'     => $this->dbname,
+                'show_err'     => $this->show_err,
+                'err_type'     => $this->err_type,
+                'last_query'   => $this->last_query,
+                'last_values'  => $this->last_values,
+                'last_results' => $this->last_results,
+            ]
+        );
+
         return $ret;
     }
 
@@ -53,25 +66,32 @@ class bdd {
      * Afficher les erreurs permet de débugguer plus rapidement les requêtes en mode développement. A désactiver en mode production
      *
      * @param boolean $err Affiche les erreurs ou non
-     * @param string $type Change le type d'erreur. Valeurs possible : 'fatal', 'warning', 'notice'
+     * @param string  $type Change le type d'erreur. Valeurs possible : 'fatal', 'warning', 'notice'
      */
-    public function initErr($err = false, $type = 'fatal') {
+    public function initErr($err = false, $type = 'fatal')
+    {
         $this->show_err = $err == true ? true : false;
-        if        ($type === 'warning')    { $this->err_type = E_USER_WARNING; }
-        elseif    ($type === 'fatal')        { $this->err_type = E_USER_ERROR; }
-        elseif    ($type === 'notice')    { $this->err_type = E_USER_NOTICE; }
-        else                            { $this->err_type = E_USER_WARNING; }
+        if ($type === 'warning') {
+            $this->err_type = E_USER_WARNING;
+        } elseif ($type === 'fatal') {
+            $this->err_type = E_USER_ERROR;
+        } elseif ($type === 'notice') {
+            $this->err_type = E_USER_NOTICE;
+        } else {
+            $this->err_type = E_USER_WARNING;
+        }
     }
 
     /**
      * Affiche les erreurs, renvoie le détail selon les paramètres envoyés  la méthode
      *
      * @param PDOException $e Utilisé en cas de throw exception sur une requête
-     * @param string $req_qry Utilisé pour afficher la requête en cas d'erreur, notamment sur les méthodes req(), row() et noRes()
+     * @param string       $req_qry Utilisé pour afficher la requête en cas d'erreur, notamment sur les méthodes req(), row() et noRes()
      * @param PDOStatement $req Contient la ressource PDOStatement permettant l'extraction d'erreurs
-     * @param boolean $trigger Si true, on renvoie l'erreur. Sinon, on affiche simplement qu'une erreur est survenue. Cela permet, en cas de paramétrage d'erreurs "fatales", d'arrêter le script en cas de besoin
+     * @param boolean      $trigger Si true, on renvoie l'erreur. Sinon, on affiche simplement qu'une erreur est survenue. Cela permet, en cas de paramétrage d'erreurs "fatales", d'arrêter le script en cas de besoin
      */
-    public function showErr($e = null, $req_qry = null, $req = null, $trigger = false) {
+    public function showErr($e = null, $req_qry = null, $req = null, $trigger = false)
+    {
         global $_PAGE;
         $final = '*|*|*Date=>'.json_encode(date(DATE_RFC822));
         $trace = is_object($e) ? $e->getTrace() : '';
@@ -84,9 +104,9 @@ class bdd {
         $final .= '||PDO caught exceptions=>'.json_encode(is_object($e) ? $e->getMessage() : '');
 
         $final .= '||Ip=>'.json_encode($_SERVER['REMOTE_ADDR'])
-        .'||Page.get=>'.json_encode($_PAGE['get'])
-        .'||Page.request=>'.json_encode($_PAGE['request'])
-        .'||User.id=>'.json_encode(Session::read('user'));
+            .'||Page.get=>'.json_encode($_PAGE['get'])
+            .'||Page.request=>'.json_encode($_PAGE['request'])
+            .'||User.id=>'.json_encode(Session::read('user'));
 
         $error_file = ROOT.DS.'logs'.DS.'sql'.DS.date('Y.m.d').'.log';
         if (!is_dir(dirname($error_file))) {
@@ -98,7 +118,7 @@ class bdd {
         fclose($f);
         echo '<pre>Une erreur MySQL est survenue...</pre>';
         if (P_DEBUG) {
-            echo'<pre>', $e->getMessage(), '</pre>';
+            echo '<pre>', $e->getMessage(), '</pre>';
         }
     }
 
@@ -106,15 +126,17 @@ class bdd {
      * Alias statique de buildReq() : Formate la requête
      *
      * @param string $req_qry La requête initiale
-     * @param array $values Les paramètres de la requête à préparer
+     * @param array  $values Les paramètres de la requête à préparer
+     *
      * @return string Requête formatée
      */
-    public static function sbuildReq($req_qry, $values = array()) {
+    public static function sbuildReq($req_qry, $values = [])
+    {
         $values = (array) $values;
         if (strpos($req_qry, '%%%fields') !== false) {//Transforme %%%Fields en une liste des champs à entrer
-            $fields = array();
+            $fields = [];
             foreach ($values as $field => $value) {
-                $field = str_replace(':','',$field);
+                $field = str_replace(':', '', $field);
                 $fields[] = '%'.$field.' = :'.$field;
             }
             $req_qry = str_replace('%%%fields', implode(', ', $fields), $req_qry);
@@ -127,11 +149,15 @@ class bdd {
                 $req_qry = str_replace('%%%in', $str, $req_qry);
             }
         }
-        $req_qry = preg_replace('#%%([a-zA-Z0-9_]+)#', ' `'.self::$prefix.'$1` ', $req_qry); // Transforme %%table en `prefix_table`
+        $req_qry = preg_replace(
+            '#%%([a-zA-Z0-9_]+)#',
+            ' `'.self::$prefix.'$1` ',
+            $req_qry
+        ); // Transforme %%table en `prefix_table`
         $req_qry = preg_replace('#%([a-zA-Z0-9_]+)#', ' `$1` ', $req_qry); // Transforme %champ en `champ`
 
-        $t = array();
-        foreach($values as $k => $v) {
+        $t = [];
+        foreach ($values as $k => $v) {
             if (!preg_match('#^:#isUu', $k) && !is_numeric($k)) {
                 unset($values[$k]);
                 $values[':'.$k] = $v;
@@ -142,10 +168,12 @@ class bdd {
         $req_qry = str_replace("\r", '', $req_qry);
         $req_qry = str_replace("    ", '', $req_qry);
         $req_qry = preg_replace('#\s\s+#Uu', ' ', $req_qry);
+
         return $req_qry;
     }
 
-    public function last_id() {
+    public function last_id()
+    {
         $last_id = 0;
         try {
             $last_id = $this->db->lastInsertId();
@@ -154,6 +182,7 @@ class bdd {
             $last_id = false;
             $this->showErr($e, '', $last_id, true);
         }
+
         return $last_id;
     }
 
@@ -161,17 +190,19 @@ class bdd {
      * Effectue une requête classique
      *
      * @param string $req_qry Une requête formatée préalablement avec buildReq()
-     * @param mixed $values Un tableau de valeurs à envoyer à PDO pour la requête préparée OU un identifiant
+     * @param mixed  $values Un tableau de valeurs à envoyer à PDO pour la requête préparée OU un identifiant
+     *
      * @return array Un tableau avec une entrée pour chaque élément trouvé dans la BDD, false
      */
-    public function req($req_qry, $values = array()) {
+    public function req($req_qry, $values = [])
+    {
         $values = (array) $values;
         $req_qry = $this->buildReq($req_qry, $values);
         $result = $this->runReq($req_qry, $values);
         if (is_object($result) && $result->rowCount() > 0) {
             $contents = $result->fetchAll();
-            foreach($contents as $key => $val) {
-                foreach($val as $vkey => $vval) {
+            foreach ($contents as $key => $val) {
+                foreach ($val as $vkey => $vval) {
                     if (is_numeric($vval)) {
                         $contents[$key][$vkey] = (int) $vval;
                     }
@@ -186,7 +217,10 @@ class bdd {
             $contents = false;
         }
         $this->last_results = $contents;
-        if (is_object($result)) { $result->closeCursor(); }
+        if (is_object($result)) {
+            $result->closeCursor();
+        }
+
         return $contents;
     }
 
@@ -194,10 +228,12 @@ class bdd {
      * Effectue une requête mais ne récupère que le premier résultat. Utile pour les sélections uniques par Id
      *
      * @param string $req_qry Une requête formatée préalablement avec buildReq()
-     * @param mixed $values Un tableau de valeurs à envoyer à PDO pour la requête préparée
+     * @param mixed  $values Un tableau de valeurs à envoyer à PDO pour la requête préparée
+     *
      * @return array à 1 entrée, false sinon
      */
-    public function row($req_qry, $values = array()) {
+    public function row($req_qry, $values = [])
+    {
         $values = (array) $values;
         $req_qry = $this->buildReq($req_qry, $values);
         if (!preg_match('#LIMIT +[0-9]+( *, *[0-9]+)?#isU', $req_qry)) {
@@ -206,7 +242,7 @@ class bdd {
         $result = $this->runReq($req_qry, $values);
         if (is_object($result) && $result->rowCount() > 0) {
             $contents = $result->fetch();
-            foreach($contents as $key => $val) {
+            foreach ($contents as $key => $val) {
                 if (is_numeric($val)) {
                     $contents[$key] = (int) $val;
                 }
@@ -220,7 +256,10 @@ class bdd {
             $contents = false;
         }
         $this->last_results = $contents;
-        if (is_object($result)) { $result->closeCursor(); }
+        if (is_object($result)) {
+            $result->closeCursor();
+        }
+
         return $contents;
     }
 
@@ -228,10 +267,12 @@ class bdd {
      * Effectue une requête mais ne récupère pas de résultat autre que la réussite ou l'échec. Utile pour update,insert,set,delete...
      *
      * @param string $req_qry Une requête formatée préalablement avec buildReq()
-     * @param mixed $values Un tableau de valeurs à envoyer à PDO pour la requête préparée
+     * @param mixed  $values Un tableau de valeurs à envoyer à PDO pour la requête préparée
+     *
      * @return true si la requête est excéutée, false sinon
      */
-    public function noRes($req_qry, $values = array()) {
+    public function noRes($req_qry, $values = [])
+    {
         $values = (array) $values;
         $req_qry = $this->buildReq($req_qry, $values);
         $result = $this->runReq($req_qry, $values);
@@ -243,7 +284,10 @@ class bdd {
             $results = false;
         }
         $this->last_results = $results;
-        if ($results) { $result->closeCursor(); }
+        if ($results) {
+            $result->closeCursor();
+        }
+
         return $results ? true : false;
     }
 
@@ -251,23 +295,29 @@ class bdd {
      * Formate la requête
      *
      * @param string $req_qry La requête initiale
-     * @param array $values Les paramètres de la requête à préparer
+     * @param array  $values Les paramètres de la requête à préparer
+     *
      * @return string Requête formatée
      */
-    private function buildReq($req_qry, $values = array()) {
+    private function buildReq($req_qry, $values = [])
+    {
         $req_qry = self::sbuildReq($req_qry, $values);
         $this->last_query = $req_qry;
         $this->last_values = $values;
+
         return $req_qry;
     }
 
     /**
      * Prépare une requête et l'exécute via PDO
+     *
      * @param string $req_qry Requête SQL (Doit avoir été formatée par la méthode buildReq)
-     * @param array $values Tableau de valeurs à envoyer à PDO pour l'exécution
+     * @param array  $values Tableau de valeurs à envoyer à PDO pour l'exécution
+     *
      * @return object PDOStatement
      */
-    private function runReq($req_qry, $values = array()) {
+    private function runReq($req_qry, $values = [])
+    {
         $values = (array) $values;
         try {
             $result = $this->db->prepare($req_qry);
@@ -277,7 +327,8 @@ class bdd {
             $this->showErr($e, $req_qry, $result, true);
         }
         $this->queriesRunnedCount++;
-        $this->queriesRunned[] = $req_qry . ' ['.implode(',', $values).']';
+        $this->queriesRunned[] = $req_qry.' ['.implode(',', $values).']';
+
         return $result;
     }
 
