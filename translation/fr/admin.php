05b0b8450dd12668dbc8ef637556cd6391c39efe
Alex Rock Ancelet <pierstoval@gmail.com>
2018-06-02 02:35:19 +0200

    Fix versions translations

--- translation/fr/admin.php
+++ translation/fr/admin.php
@@ -1,22 +1,27 @@
 <?php return array (
-  0 => 
+  0 =>
   array (
     'source' => 'Bienvenue sur le panneau d\'administration',
     'trans' => 'Bienvenue sur le panneau d\'administration',
   ),
-  1 => 
+  1 =>
   array (
     'source' => 'Changer d\'utilisateur',
     'trans' => 'Changer d\'utilisateur',
   ),
-  2 => 
+  2 =>
   array (
     'source' => 'Corahn Rin',
     'trans' => 'Corahn Rin',
   ),
-  3 => 
+  3 =>
   array (
     'source' => 'Administration',
     'trans' => 'Administration',
+  ),
+  4 =>
+  array (
+    'source' => 'Erreur 404',
+    'trans' => 'Error 404',
   ),
 );
