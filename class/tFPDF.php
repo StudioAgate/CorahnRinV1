2b93cc71928ac4ce25caf05a16b50b548dde1eed
Alex Rock Ancelet <pierstoval@gmail.com>
2020-12-18 13:52:55 +0100

    Remove deprecation

--- class/tFPDF.php
+++ class/tFPDF.php
@@ -872,10 +872,6 @@ class tFPDF extends FPDF
         if (ini_get('mbstring.func_overload') & 2) {
             $this->Error('mbstring overloading must be disabled');
         }
-        // Ensure runtime magic quotes are disabled
-        if (get_magic_quotes_runtime()) {
-            @set_magic_quotes_runtime(0);
-        }
     }
 
     public function _getfontpath()
