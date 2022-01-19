7c3bc82c886ad440ffe4b0486ba297196d3d5cba
Alex Rock Ancelet <pierstoval@gmail.com>
2022-01-19 15:43:22 +0100

    Update docker dev setup

--- docker/php/bin/entrypoint.sh
+++ docker/php/bin/entrypoint.sh
@@ -1,26 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 uid=$(stat -c %u /srv)
 gid=$(stat -c %g /srv)
 
-if [ ${uid} == 0 ] && [ ${gid} == 0 ]; then
-    if [ $# -eq 0 ]; then
-        php-fpm
-    else
-        exec "$@"
-        exit
-    fi
+if [[ "$@" == "" ]]; then
+    php-fpm7.4
+    exit $?
 fi
 
-sed -i "s/user = www-data/user = foo/g" /usr/local/etc/php-fpm.d/www.conf
-sed -i "s/group = www-data/group = foo/g" /usr/local/etc/php-fpm.d/www.conf
-sed -i -r "s/foo:x:\d+:\d+:/foo:x:$uid:$gid:/g" /etc/passwd
-sed -i -r "s/foo:x:\d+:/foo:x:$gid:/g" /etc/group
-chown foo /home
+sed -i "s/user = www-data/user = ${RUN_USER}/g" /etc/php/${PHP_VERSION}/fpm/php-fpm.conf
+sed -i "s/group = www-data/group = ${RUN_USER}/g" /etc/php/${PHP_VERSION}/fpm/php-fpm.conf
+sed -i -r "s/${RUN_USER}:x:\d+:\d+:/${RUN_USER}:x:$uid:$gid:/g" /etc/passwd
+sed -i -r "s/${RUN_USER}:x:\d+:/${RUN_USER}:x:$gid:/g" /etc/group
+chown ${RUN_USER} /home
+
+mkdir -p vendor
+chown -R ${RUN_USER}:${RUN_USER} /srv/
 
 if [ $# -eq 0 ]; then
     php-fpm
 else
-    exec su-exec foo "$@"
+    exec gosu ${RUN_USER} "$@"
 fi
